eff4ff46724f6c912f5974b00876496258346307
ahmed <ahmedlearn01@gmail.com>
2021-06-30 07:49:13 -0700

    initial commit

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 const Home = () =>{
+    const [name,setName] = useState('Ahmed');
+    const [age,setAge] = useState(23);
+
+    const ya = (Mname,Mage) =>{
+        setName(Mname);
+        setAge(Mage)
+    }
     return (
         <div className="content">
+            <p>hey I'm {name} {age} year old, and that means more !!!</p>
             <h1>Home Page</h1>
+            <button onClick={()=>ya('Nur',18)}>click me</button>
         </div>
     )
 }

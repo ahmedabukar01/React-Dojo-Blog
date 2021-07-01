fe68cfb80e0977f97f3f651b33ebcefcb9c2dbe4
ahmed <ahmedlearn01@gmail.com>
2021-07-01 15:14:21 -0700

    catching errors

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,8 @@ import BlogList from "./BlogList";
 
 const Home = () =>{
     const [blogs,setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     const handleClick =(id)=>{
         const newBlogs = blogs.filter(blog=>blog.id !== id);
@@ -10,17 +12,28 @@ const Home = () =>{
     }
     
     useEffect(()=>{
-        fetch('http://localhost:3000/blogs')
+        fetch('http://localhost:800/blogs')
         .then(data=>{
+            if(!data.ok){
+                throw Error('could not fetch the data');
+            }
             return data.json();
         })
         .then(data=>{
             setBlogs(data);
+            setIsPending(false)
+            setError(null)
         })
+        .catch(err=>{
+            setError(err.message);
+            setIsPending(false)
+        });
     }, [])
 
     return (
         <div className="content">
+            {error && <div>{error}</div>}
+            {isPending && <div>loading...</div>}
          {blogs && <BlogList blogs={blogs} title="All Blogs" handleClick={handleClick}/>}
         </div>
     )

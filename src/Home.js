8cd4b390e3facbe4809e58a579283ad55ee9be2d
ahmed <ahmedlearn01@gmail.com>
2021-07-03 09:52:01 -0700

    initial commit

--- src/Home.js
+++ src/Home.js
@@ -1,41 +1,15 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () =>{
-    const [blogs,setBlogs] = useState(null)
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    const handleClick =(id)=>{
-        const newBlogs = blogs.filter(blog=>blog.id !== id);
-        setBlogs(newBlogs);
-    }
-    
-    useEffect(()=>{
-        fetch('http://localhost:800/blogs')
-        .then(data=>{
-            if(!data.ok){
-                throw Error('could not fetch the data');
-            }
-            return data.json();
-        })
-        .then(data=>{
-            setBlogs(data);
-            setIsPending(false)
-            setError(null)
-        })
-        .catch(err=>{
-            setError(err.message);
-            setIsPending(false)
-        });
-        
-    }, [])
+    const {data: blogs,isPending,error} = useFetch('http://localhost:800/blogs');
 
     return (
         <div className="content">
             {error && <div>{error}</div>}
             {isPending && <div>loading...</div>}
-         {blogs && <BlogList blogs={blogs} title="All Blogs" handleClick={handleClick}/>}
+         {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     )
 }
---
+++ src/useFetch.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+const useFetch = (url)=>{
+    const [data,setData] = useState(null)
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+    
+    useEffect(()=>{
+        fetch(url)
+        .then(data=>{
+            if(!data.ok){
+                throw Error('could not fetch the data');
+            }
+            return data.json();
+        })
+        .then(data=>{
+            setData(data);
+            setIsPending(false)
+            setError(null)
+        })
+        .catch(err=>{
+            setError(err.message);
+            setIsPending(false)
+        });
+        
+    }, [])
+
+    return {data,isPending,error};
+}
+
+export default useFetch;

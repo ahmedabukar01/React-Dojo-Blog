7088291a0cbf06d49f1194df73f54209992d3c57
ahmed <ahmedlearn01@gmail.com>
2021-07-16 20:19:29 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -18,3 +18,7 @@ export default App;
 // paused until this month, yes it is
 // i will be back working this project after a month.
 // making all these comments for just github.
+
+// paused until this month, yes it is
+// i will be back working this project after a month.
+// making all these comments for just github.

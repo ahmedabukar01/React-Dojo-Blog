b3e45600a73863cca0218dd5ae9610cad2037110
ahmed <ahmedlearn01@gmail.com>
2021-11-14 11:43:56 -0800

    blog details

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import {useParams} from 'react-router-dom';
+import useFetch from './useFetch';
 
 function BlogDetails() {
-    const {id} = useParams()
+    const {id} = useParams();
+    const {data,isPending} = useFetch(`http://localhost:5000/blogs/${id}`);
+
     return (
         <div className="blog-details">
-            <h2>Blog Detail {id} </h2>
+            {isPending && <p>Loading...</p>}
+           { data && (
+               <article>
+                   <h2>{data.title}</h2>
+                   <p>Written by {data.author}</p>
+                   <div>{data.body}</div>
+               </article>
+           ) }
         </div>
     )
 }

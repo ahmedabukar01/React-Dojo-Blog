bfe8bc974051139268ba73b7e11ec4a84d4290dc
ahmed <ahmedlearn01@gmail.com>
2021-11-14 17:28:25 -0800

    initial commit

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,40 @@
+import { useState } from 'react';
+
 const Create = () =>{
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('')
+    const [author, setAuthor] = useState('');
+
     return (
         <div className="create">
             <h1>Create new Blog...</h1>
+            <form>
+                <label>Blog Title</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e)=>setTitle(e.target.value)}
+                ></input>
+                <label>Blog Body</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e)=>setBody(e.target.value)}
+                ></textarea>
+                <label>Author</label>
+                <select
+                    value={author}
+                    onChange={(e)=>setAuthor(e.target.value)}
+                >
+                    <option value="ahmed">Ahmed</option>
+                    <option value="Abukar">Abukar</option>
+                </select>
+                <button>Add Blog</button>
+                <p>{author}</p>
+                <p>{body}</p>
+                <p>{title}</p>
+            </form>
         </div>
     )
 }
